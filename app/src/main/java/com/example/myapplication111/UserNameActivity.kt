bc8332912d47aff5c89cf1a04a56c40acee7c298
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-07 10:49:51 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication111/UserNameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication111
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_name)
+    }
+}
