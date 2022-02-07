d8dc2e76755861dc6014176dd0484b050a6f60fc
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-07 18:17:44 +0400

    activities_4

--- app/src/main/java/com/example/myapplication111/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication111/SecondActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication111
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+       // val arguments = intent.extras  ловим данные из предыдущей activity
+       // val data = arguments?.get("result").toString()
+
+        val button = findViewById<AppCompatButton>(R.id.button3)
+
+        button.setOnClickListener(object : View.OnClickListener {//здесь возврат в предыдущую activity
+            override fun onClick(v: View?) {
+              //  intent.putExtra("result", editText_1.text.toString() + editText_2.text.toString())
+             //   startActivity(intent)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/myapplication111/UserNameActivity.kt
+++ app/src/main/java/com/example/myapplication111/UserNameActivity.kt
@@ -1,11 +1,34 @@
 package com.example.myapplication111
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
 
 class UserNameActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_name)
+
+        val editText_1 = findViewById<EditText>(R.id.editTextTextPersonName1)
+        val editText_2 = findViewById<EditText>(R.id.editTextTextPersonName2)
+
+        val button1 = findViewById<AppCompatButton>(R.id.button1)
+        val button2 = findViewById<AppCompatButton>(R.id.button2)
+
+        val intent = Intent(this, SecondActivity::class.java)
+
+        button2.setOnClickListener(object : View.OnClickListener { // описать события при вводе информации и сохранении
+            override fun onClick(v: View?) {
+               // intent.putExtra("result", editText_1.text.toString() + editText_2.text.toString())
+                startActivity(intent)
+            }
+        })
     }
+
+    //предусмотреть ситуацию, что activity может упасть, сделать восстановление состояния
 }
